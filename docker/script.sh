f5176029c7100d50e7251fb08a7fe9563ce54410
Your Name <you@example.com>
2023-04-10 18:48:30 +0200

    start docker build

---
+++ docker/script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+_fanState="off"
+_fanOnTemperature="40000"
+_fanOffTemperature="35000"
+
+while true; do
+    _temperature="$(cat /sys/class/thermal/thermal_zone0/temp)"
+
+    _temperatureViewAwk="BEGIN { printf \"%.2f C\", (${_temperature}/1000) }"
+    _temperatureView=$(awk "${_temperatureViewAwk}";)
+
+    if (( "${_temperature}" > "${_fanOnTemperature}" )); then
+	_fanState="on"
+    elif (( "${_temperature}" < "${_fanOffTemperature}" )); then
+	_fanState="off"
+    fi
+
+    ./waveshare-poe-hat-b-cli fan ${_fanState}
+    ./waveshare-poe-hat-b-cli oled "T: ${_temperatureView}" "F: ${_fanState}"
+    sleep 3
+done
